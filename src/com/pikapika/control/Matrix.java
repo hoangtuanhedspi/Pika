3bba921c18db904119110a65d1547e1be16637f4
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-05-21 22:27:23 +0700

    Update more pikachu Icon

--- src/com/pikapika/control/Matrix.java
+++ src/com/pikapika/control/Matrix.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pikapika.control.Matrix;
+package com.pikapika.control;
 
 import com.pikapika.view.Pikachu;
-import java.awt.Point;
 import java.util.Random;
 
 /**
