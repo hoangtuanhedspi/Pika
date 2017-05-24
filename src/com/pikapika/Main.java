c59c40887dc0a5283019959d511631d37649f542
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-05-24 21:07:05 +0700

    Finish Pika Project

--- src/com/pikapika/Main.java
+++ src/com/pikapika/Main.java
@@ -1,5 +1,12 @@
 package com.pikapika;
 import com.pikapika.control.GameController;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static com.pikapika.utils.Utils.WINDOW_HEIGHT;
 import static com.pikapika.utils.Utils.WINDOW_WIDTH;
 
@@ -7,6 +14,7 @@ public class Main{
     public static void main(String[] args) {
         GameController pikachu = new GameController("Pikachu");
         pikachu.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+        pikachu.setLocationRelativeTo(null);
         pikachu.start();
     }
 }
